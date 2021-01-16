97f2cfff0a78503c55a52a5fe951c98adf44f681
Daniel Lim <daniellim@Daniels-MacBook-Pro.local>
2021-01-15 20:15:17 -0800

    duplicated css style removed

--- src/components/searchBox/SearchBox.test.js
+++ src/components/searchBox/SearchBox.test.js
@@ -1,10 +1,10 @@
-import App from "./App";
 import { configure, shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import SearchBox from "./SearchBox";
 
 configure({ adapter: new Adapter() });
 
 it("renders without crashing", () => {
   // shallow only checks App component, not child component belongs to App component.
-  shallow(<App />);
+  shallow(<SearchBox />);
 });
